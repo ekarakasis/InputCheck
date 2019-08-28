5724643925a4bf8c3b20054880415c8e040b1ea1
ekarakasis <evangelos.karakasis@gmail.com>
2019-08-28 03:39:18 +0300

    build: updates the version in the setup file

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
       name         = "InputCheck",
-      version      = '0.1.0',
+      version      = '0.3.1',
       description  = "Checks the validity of functions inputs",
       url          = "https://github.com/ekarakasis/InputCheck",
       author       = "Evangelos G. Karakasis",
